d536881ab5cdaffd429ecb14a76d6e51259066a5
madhavanthangaraj <madhavanthangaraj24@gmail.com>
2025-07-14 14:59:44 +0530

    fn class work committed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,54 @@
 import { useState } from "react";
 import axios from "axios";
+import "./Login.css";
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  async function handleLogin(event){
+
+  async function handleLogin(event) {
     event.preventDefault();
-    try{
-        const token = await axios.post("http://localhost:3001/api/auth/login",{userName,password})
-        console.log(token);
-        alert("Login Successful")
-    } catch (e){
-        console.log("Login Error", e);
-        alert("Invalid Cred")
+    try {
+      const response = await axios.post("http://localhost:3001/api/auth/login", {
+        userName,
+        password,
+      });
+      console.log(response.data);
+      alert("Login Successful");
+    } catch (e) {
+      console.error("Login Error", e);
+      alert("Invalid Credentials");
     }
-    console.log("Form Submitted");
   }
+
   return (
-    <div>
+    <div className="login-container">
       <h2>Login</h2>
-      <div>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="userName">User Name</label>
-          <input
-            id="userName"
-            name="userName"
-            value={userName}
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <br /> <br />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <br />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="userName">User Name</label>
+        <input
+          id="userName"
+          name="userName"
+          value={userName}
+          type="text"
+          placeholder="Enter your username"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
+
 export default Login;
