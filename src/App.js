3c1e603851d4578a68050ba32124988232049814
madhavanthangaraj <madhavanthangaraj24@gmail.com>
2025-07-16 20:40:28 +0530

    task committed success

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import LoginForm from './components/LoginForm';
 import Header from './components/Header';
 import Logout from './components/Logout';
 import HomePage from './components/HomePage';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
   return (
     <Router>
-      {window.location.pathname !== '/home' && <Header />}
+      {window.location.pathname !== '/' && <Header />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
       </Routes>
     </Router>
   );
